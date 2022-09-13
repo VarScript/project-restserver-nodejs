9bb636c2064ca9becc15053bca6df44e18612828
DevHars <vargassanchezfabio01@gmail.com>
2022-09-13 10:02:29 -0500

    login user

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,52 @@
 const { response } = require("express");
+const bcryptjs = require('bcryptjs')
 
-const login = (req, res = response) => {
+const User = require('../models/user')
+
+const login = async (req, res = response) => {
+
+    const { email, password } = req.body;
+
+    try {
+        
+        // Verify if the Email exist
+        const user = await User.findOne({ email })
+        if ( !user ) {
+            return res.status(400).json({
+                msg: 'User / Password are not corrects - Email'
+            })
+        }
+
+        // If the user is active
+        if ( !user.status ) {
+            return res.status(400).json({
+                msg: 'User / Password are not corrects - status: false'
+            })
+        }
+
+        // Verify the password
+        const validatePassword = bcryptjs  .compareSync( password, user.password) 
+        if ( !validatePassword ){
+            return res.status(400).json({
+                msg: 'User / Password are not corrects - password'
+            })
+        }
+
+        // Generate the JWT
+        
 
 
-    res.json({
-        msg: 'Login ok'
-    });
+        res.json({
+            msg: 'Login ok'
+        });
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            msg: 'something went wrong'
+        });
+    }
+
 }
 
 
