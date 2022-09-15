8485b1c742d27f6b3f2d82fae86a9641a7e8e34c
DevHars <vargassanchezfabio01@gmail.com>
2022-09-15 09:19:10 -0500

    Category model

---
+++ models/category.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+
+
+
+const CategorySchema = Schema ({
+    name: {
+        type: String,
+        require: [true, 'The name is require']
+    },
+    status: {
+        type: Boolean,
+        default: true,
+        require: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        require: true
+    }
+});
+
+
+
+module.exports = model( 'Category', CategorySchema );
