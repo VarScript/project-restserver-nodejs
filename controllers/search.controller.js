6a78dc7d57defeed13a74eb1c69735a590048f99
DevHars <vargassanchezfabio01@gmail.com>
2022-09-16 10:52:46 -0500

    Finish searches

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -2,12 +2,13 @@ const { response } = require("express");
 
 const { ObjectId } = require('mongoose').Types;
 
-const { User, Category, Product } = require('../models')
+const { User, Category, Product } = require('../models');
+const product = require("../models/product");
 
 const permittedCollection = [
     'users',
-    'category',
-    'product',
+    'categories',
+    'products',
     'role'
 ];
 
@@ -17,16 +18,9 @@ const searchUsers = async ( term = '', res = response) => {
 
     if ( isMongoId ){
         const user = await User.findById( term );
-        if (user.status === false) {
-            return res.json({
-                msg: 'Ther user not exist'
-            });
-        } else {
-            return res.json({
-                result: ( user ) ? [ user ] : []
-            });
-        }
         
+        if (user.status === false) return res.json({ result: [] });
+        else return res.json({ result: ( user ) ? [ user ] : [] });
     }   
     
     // RegExp is a regular expression that serve for that be an insensitive to the capital latter and lower case
@@ -40,9 +34,55 @@ const searchUsers = async ( term = '', res = response) => {
         $and: [{ status: true }]
     });
     res.json({result: usersC, users});
-
-
 }
+
+
+
+const searchCategories = async ( term = '', res = response) => {
+
+    const isMongoId = ObjectId.isValid( term ); 
+
+    if ( isMongoId ){
+        const category = await Category.findById( term ).populate('user', 'name');
+
+        if (category.status === false) return res.json({ result: [] });
+        else return res.json({ result: category });
+    }   
+    
+    // RegExp is a regular expression that serve for that be an insensitive to the capital latter and lower case
+    const regex = new RegExp(term, 'i')
+    const categoryC = await Category.count({ name: regex, status: true })
+                            .populate('user', 'name');
+    const category = await Category.find({ name: regex, status: true })
+                            .populate('user', 'name');
+
+    res.json({result: categoryC, category});
+}
+
+
+
+const searchProducts = async ( term = '', res = response) => {
+
+    const isMongoId = ObjectId.isValid( term ); 
+
+    if ( isMongoId ){
+        const product = await Product.findById( term ).populate('category', 'name');
+
+        if (product.status === false) return res.json({ result: [] })
+        else return res.json({ result:product });
+    }   
+    
+    // RegExp is a regular expression that serve for that be an insensitive to the capital latter and lower case
+    const regex = new RegExp(term, 'i')
+    const productC = await Product.count({  name: regex ,status: true })
+                            .populate('category', 'name');
+    const product = await Product.find({  name: regex ,status: true })
+                            .populate('category', 'name');
+
+    res.json({result: productC, product});
+}
+
+
 
 
 const search = (req, res= response) => {
@@ -59,11 +99,11 @@ const search = (req, res= response) => {
         case 'users':
             searchUsers(term, res)
         break;
-        case 'category':
-        
+        case 'categories':
+            searchCategories(term, res)
         break;
-        case 'product':
-        
+        case 'products':
+            searchProducts(term, res)
         break;
 
         default:
